89b42ea633a3099c5d2c9ceb474e69cb481eb14e
Lian Kai <liankai@gmail.com>
2023-09-09 13:36:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -17,4 +19,18 @@ def best():
 @app.route('/sc')
 def sc():
     name = 'John Doe'
-    return render_template('sc.html', name=name)
+    
+    # Send a GET request to the URL
+    url = 'http://www.weather.com.cn/weather1d/101020100.shtml'
+    response = requests.get(url)
+    
+    # Create a BeautifulSoup object to parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find the element containing the temperature value
+    temperature_element = soup.find(class_='tem')
+    
+    # Extract the temperature value from the element
+    temperature = temperature_element.text.strip()
+
+    return render_template('sc.html', name=temperature)
